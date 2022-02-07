86b481d08648aab5a40f3c4d40d150c220414745
Carlos Abraham Pérez Marrero <abrahampm96@gmail.com>
2022-02-07 13:40:26 -0500

    Create Alten Test Bookin API Project

---
+++ src/app/app.module.ts
@@ -0,0 +1,120 @@
+// @angular imports
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+
+// Header components imports
+import { MainHeaderComponent } from './components/main-header/main-header.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+
+// Room components imports
+import { RoomCreateComponent } from './components/room-create/room-create.component';
+import { RoomEditComponent } from './components/room-edit/room-edit.component';
+import { RoomFormComponent } from './components/room-form/room-form.component';
+import { RoomListComponent } from './components/room-list/room-list.component';
+import { RoomService } from './services/room.service';
+
+// Reservation components imports
+import { ReservationCreateComponent } from './components/reservation-create/reservation-create.component';
+import { ReservationEditComponent } from './components/reservation-edit/reservation-edit.component';
+import { ReservationListComponent } from './components/reservation-list/reservation-list.component';
+import { ReservationFormComponent } from './components/reservation-form/reservation-form.component';
+import { ReservationService } from './services/reservation.service';
+
+// Flex layout import
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+// Material modules imports
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import {MAT_DATE_LOCALE, MatNativeDateModule} from '@angular/material/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+
+// Rich text editor CKEditor module import
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+
+// Auxiliary component and service to show dialog when performing server requests
+import { LoadingDialogComponent } from './dialogs/loading-dialog/loading-dialog.component';
+import { DialogService } from './dialogs/dialog.service';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import {AuthService} from './auth/auth.service';
+import { ReservationAvailabilityComponent } from './components/reservation-availability/reservation-availability.component';
+import {MatCardModule} from '@angular/material/card';
+import {AuthInterceptor, DEFAULT_TIMEOUT} from './auth/auth.interceptor';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainHeaderComponent,
+    PageHeaderComponent,
+    ReservationListComponent,
+    ReservationCreateComponent,
+    ReservationEditComponent,
+    ReservationFormComponent,
+    RoomCreateComponent,
+    RoomListComponent,
+    RoomEditComponent,
+    RoomFormComponent,
+    LoadingDialogComponent,
+    LoginComponent,
+    RegisterComponent,
+    ReservationAvailabilityComponent,
+  ],
+  imports: [
+    // Angular modules
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    FlexLayoutModule,
+    // Material modules
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDialogModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatSortModule,
+    MatTableModule,
+    // Rich text editor module
+    CKEditorModule,
+    ReactiveFormsModule,
+  ],
+  // No entry component declaration needed since Angular 9.0.0,
+  // Therefore there is not need to add LoadingDialogComponent to entryComponents array
+  providers: [
+    AuthService,
+    ReservationService,
+    RoomService,
+    DialogService,
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: DEFAULT_TIMEOUT, useValue: 60000}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
