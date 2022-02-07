8a2891e82822f54270747879b4cf5d79363687d1
Carlos Abraham Pérez Marrero <abrahampm96@gmail.com>
2022-02-07 13:45:23 -0500

    Create Dialog Service

---
+++ src/app/dialogs/dialog.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { LoadingDialogComponent } from './loading-dialog/loading-dialog.component';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DialogService {
+  loadingDialogRef: MatDialogRef<any>;
+  loadingDialogData: BehaviorSubject<any>;
+
+  constructor(private dialog: MatDialog) {
+    this.loadingDialogData = new BehaviorSubject<any>(null);
+    this.loadingDialogData.subscribe();
+  }
+
+  openLoadingDialog(text= 'Loading', status = 'LOADING') {
+    this.loadingDialogData.next({text, status});
+    this.loadingDialogRef = this.dialog.open(LoadingDialogComponent, {
+      data: {dataSubject: this.loadingDialogData},
+      disableClose: true,
+    });
+  }
+
+  updateLoadingDialogData(text: string, status: string) {
+    this.loadingDialogData.next({text, status});
+  }
+
+  closeLoadingDialog() {
+    this.loadingDialogRef.close();
+  }
+
+  closeLoadingDialogAfterTimeout(timeout= 3000) {
+    setTimeout(() => {
+      this.closeLoadingDialog();
+    }, timeout);
+  }
+}
