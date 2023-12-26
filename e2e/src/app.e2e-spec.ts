7eabe49670ffa7f94f37c278dd52d872c35e0da2
Carlos Abraham Pérez Marrero <abrahampm96@gmail.com>
2023-12-26 14:16:28 -0500

    Update config

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('alten-test app is running!');
+    expect(page.getTitleText()).toEqual('demo-booking-app app is running!');
   });
 
   afterEach(async () => {
